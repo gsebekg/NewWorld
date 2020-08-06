5173e95d90527e2f52a2236ecde559be751c678f
Sebastian Grzesiak <gsebekg3@wp.pl>
2020-08-06 18:38:14 +0200

    Pierwsze wgranie

---
+++ NewWorld/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NewWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Authorize]
+        public ActionResult GameList()
+        {
+            ViewBag.Message = "Lista Gier.";
+
+            return View();
+        }
+    }
+}
